55ee9510008384943786348062b218029d93d0f9
Emil Erlandsson <emil@buglix.org>
2011-02-16 11:45:53 +0100

    Merged in the bash utility functions to make it more portable

--- pyplcli/pyplcli.py
+++ pyplcli/pyplcli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-untitled.py
+pyplcli.py
 
 Created by Emil Erlandsson on 2011-02-15.
 Copyright (c) 2011 Procera Networks. All rights reserved.
@@ -11,7 +11,6 @@ Copyright (c) 2011 Procera Networks. All rights reserved.
 # * Web update via MD5(ing) github and compare. Possibly through a thread.
 # * dump/load NetObject tree states, including dynitems and tree in ASCII form
 # * Add support for relative paths to cd
-
 
 import atexit
 import sys
@@ -24,12 +23,111 @@ import md5
 import difflib
 
 import packetlogic2
-from bash import Colors
 
 CONNECTIONS_FILE = os.path.join(os.environ["HOME"], ".pyplcli_connections")
 HISTORY_FILE     = os.path.join(os.environ["HOME"], ".pyplcli_history")
 
 SCRIPT_URL = "https://github.com/emilerl/emilerl/raw/master/pyplcli/pyplcli.py"
+
+# Bash utility functions
+RESET = '\033[0m'
+CCODES = {
+    'black'           :'\033[0;30m',
+    'blue'            :'\033[0;34m',
+    'green'           :'\033[0;32m',
+    'cyan'            :'\033[0;36m',
+    'red'             :'\033[0;31m',
+    'purple'          :'\033[0;35m',
+    'brown'           :'\033[0;33m',
+    'light_gray'      :'\033[0;37m',
+    'dark_gray'       :'\033[0;30m',
+    'light_blue'      :'\033[0;34m',
+    'light_green'     :'\033[0;32m',
+    'light_cyan'      :'\033[0;36m',
+    'light_red'       :'\033[0;31m',
+    'light_purple'    :'\033[0;35m',
+    'yellow'          :'\033[0;33m',
+    'white'           :'\033[0;37m',
+}
+
+MCODES = {
+    'POS_LC'            : '\033[%d;%dH',
+    'MOVE_UP_N'         : '\033[%dA',
+    'MOVE_DOWN_N'       : '\033[%dB',
+    'MOVE_FORWARD_N'    : '\033[%dC',
+    'MOVE_BACK_N'       : '\033[%dD',
+    'CLEAR'             : '\033[2J',
+    'ERASE_EOL'         : '\033[K',
+    'SAVE'              : '\033[s',
+    'RESTORE'           : '\033[u',
+}
+
+def terminal_size():
+    import fcntl, termios, struct
+    try:
+        h, w, hp, wp = struct.unpack('HHHH',
+            fcntl.ioctl(0, termios.TIOCGWINSZ,
+            struct.pack('HHHH', 0, 0, 0, 0)))
+    except IOError:
+        return 0,0
+    return w, h
+
+class Screen(object):
+    """Helper class for moving the cursor on the screen"""
+        
+    def save_position(self):
+        print MCODES["SAVE"],
+    
+    def restore_position(self):
+        print MCODES["RESTORE"],
+        
+    def move_up(self,lines = 1):
+        print MCODES["MOVE_UP_N"] % lines,
+    
+    def move_down(self, lines = 1):
+        print MCODES["MOVE_DOWN_N"] % lines,
+        
+    def move_forward(self, cols = 1):
+        print MCODES["MOVE_FORWARD_N"] % cols,
+
+    def move_backward(self, cols = 1):
+        print MCODES["MOVE_BACKWARD_N"] % cols,
+    
+    def position(self, x, y):
+        "x=line, y=column"
+        print MCODES["POS_LC"] % (x,y),
+    
+    def clear(self):
+        print MCODES["CLEAR"],
+        
+    def erase_line(self):
+        print MCODES["ERASE_EOL"],
+
+
+class Colors(object):
+    """A helper class to colorize strings"""
+    def __init__(self, state = False):
+        self.disabled = state
+    
+    def disable(self):
+        self.disabled = True
+        
+    def enable(self):
+        self.disabled = False
+            
+    def __getattr__(self,key):
+        if key not in CCODES.keys():
+            raise AttributeError, "Colors object has no attribute '%s'" % key
+        else:
+            if self.disabled:
+                return lambda x: x
+            else:
+                return lambda x: RESET + CCODES[key] + x + RESET
+    
+    def __dir__(self):
+        return self.__class__.__dict__.keys() + CCODES.keys()
+                    
+# EO: Bash utility functions
 
 connections = {}
 c = Colors()
@@ -41,7 +139,6 @@ server = None
 username = None
 password = None
 path = "/NetObjects"
-
 
 #############################################################################
 ############################  "Shell commands" ##############################
